e0d419cc74a7560c406215f533309b4d8258ac3c
olcs <sschandel304@gmail.com>
2023-06-05 17:17:10 +0530

    update in logger

--- src/exception.py
+++ src/exception.py
@@ -2,4 +2,16 @@ import sys
 
 def error_message_details(error,error_details:sys):
     _,_,exc_tab=error_details.exc_info()
+    file_name=exc_tab.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1} error message [{2}]".format() 
+    file_name,exc_tab.tb_lineno,str(error)
+
+    return(error_message)
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_message=error_message_details(error_message,error_details=error_detail)
+
+    def __str__(self):
+        return self.error_message
